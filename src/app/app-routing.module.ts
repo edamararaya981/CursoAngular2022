c9039238f9e9df9d20dbc351ce74e4f31f260df3
edamararaya981 <edam_araya981@yahoo.com.ar>
2022-11-05 02:08:59 -0300

    Version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearEmpleadosComponent } from './components/crear-empleados/crear-empleados.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'lista', pathMatch:'full'},
+  {path: 'lista', component: ListaEmpleadosComponent},
+  {path: 'crear', component: CrearEmpleadosComponent},
+  {path: 'editar/:id', component: CrearEmpleadosComponent},
+  {path:'**',redirectTo:'lista', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { CrearEmpleadosComponent } from './components/crear-empleados/crear-empleados.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+//Clases firebase
+import{AngularFireModule} from '@angular/fire/compat';
+import{AngularFireAuthModule} from '@angular/fire/compat/auth';
+import{AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+//Configuración de Firebase
+// import {environment} from 'src/environments/environment';
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadosComponent,
+    CrearEmpleadosComponent,
+    NavbarComponent,
+    FooterComponent,
+
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    //AngularFireModule,
+    // AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireAnalyticsModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideFirestore(() => getFirestore()),
+    ReactiveFormsModule,
+    // ToastrModule.forRoot(),
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
